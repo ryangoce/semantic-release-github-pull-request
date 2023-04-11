4c7dd85836786507e333cc591c88b0e12f710f6f
Ryan Goce <ryangoce@gmail.com>
2023-04-11 16:04:07 +0800

    build: added semantic release

---
+++ release.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  branches: [
+    'release'
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    ['@semantic-release/release-notes-generator', {
+      linkCompare: false,
+      linkReferences: false
+    }],
+    ['@semantic-release/changelog', {
+      changelogFile: 'CHANGELOG.md'
+    }],
+    ['@semantic-release/exec', {
+      verifyReleaseCmd: 'echo ${nextRelease.version} > VERSION.md',
+    }],
+    '@semantic-release/github',
+    [
+      'semantic-release-github-pullrequest', {
+        assets: [
+          'CHANGELOG.md',
+          'VERSION.md',
+          'RELEASE.md'
+        ],
+        baseRef: 'release'
+      }
+    ]
+  ]
+}
